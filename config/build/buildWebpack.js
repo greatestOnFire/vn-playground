2ea8fc91f3b6365935829763402c5a26147832f2
Mustakhim Omarov <77112883+greatestOnFire@users.noreply.github.com>
2024-11-25 19:59:08 +0500

    STAGE-0:chore [buildWebpack] prod build optimization

--- config/build/buildWebpack.js
+++ config/build/buildWebpack.js
@@ -1,17 +1,26 @@
-const buildLoaders = require ("./buildLoaders.js");
-const buildPlugins = require ("./buildPlugins.js");
-const buildDevServer = require ("./buildDevServer.js");
-const buildResolves = require ("./buildResolves.js");
+const buildLoaders = require('./buildLoaders.js')
+const buildPlugins = require('./buildPlugins.js')
+const buildDevServer = require('./buildDevServer.js')
+const buildResolves = require('./buildResolves.js')
 
 function buildWebpack(options) {
-    const {buildMode, paths} = options;
+    const { buildMode, paths } = options
+
+    const isDevelopment = buildMode === 'development'
+    const isProduction = buildMode === 'production'
+
+    let devTool = 'source-map'
+
+    if (isDevelopment) {
+        devTool = 'eval-source-map'
+    }
 
     return {
         mode: buildMode,
         entry: paths.entry,
         output: {
             path: paths.output,
-            filename: "[name].[contenthash].js",
+            filename: '[name].[contenthash].js',
             clean: true,
         },
         module: {
@@ -19,8 +28,9 @@ function buildWebpack(options) {
         },
         plugins: buildPlugins(options),
         resolve: buildResolves(options),
+        devtool: devTool,
         devServer: buildDevServer(options),
     }
-};
+}
 
-module.exports = buildWebpack;
+module.exports = buildWebpack
