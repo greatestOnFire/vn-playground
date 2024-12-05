a64ad4eb76d3ef0d36f91be9616b05907226bbf4
Mustakhim Omarov <77112883+greatestOnFire@users.noreply.github.com>
2024-12-05 13:57:23 +0500

    STAGE-1:feat create [src/components/UI/UIInput.vue]

---
+++ src/components/UI/UIInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <input />
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+input {
+    width: 100%;
+    padding: 8px 16px;
+    border: 1px solid;
+    border-color: var(--external-primary-color);
+    border-radius: 5px;
+    font-size: 16px;
+    font-family: 'Inter', serif;
+    font-weight: 500;
+    color: var(--external-primary-color);
+    box-sizing: border-box;
+    background-color: transparent;
+
+    &::placeholder {
+        color: inherit;
+    }
+
+    &:focus,
+    &:focus-visible {
+        outline: 1px solid var(--external-primary-color);
+    }
+}
+</style>
